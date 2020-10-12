db28ee8c97d99137c8d67128f3441c79e4c4996e
Axel de la Torre <axeldelat@gmail.com>
2020-10-12 09:15:21 -0500

    initial commit CRUD

---
+++ index.js
@@ -0,0 +1,104 @@
+const { request, response } = require('express')
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+const port = 8080
+
+let beers = [
+    {
+        id: 'abc123',
+        marca: 'corona',
+        tipo: 'pilsner',
+        precio: 30
+    },
+    {
+        id: 'abc124',
+        marca: 'Heineken',
+        tipo: 'lager',
+        precio: 22
+    },
+    {
+        id: 'abc125',
+        marca: 'Bohemia',
+        tipo: 'viena',
+        precio: 25
+    },
+    {
+        id: 'abc126',
+        marca: 'Paulaner',
+        tipo: 'lager',
+        precio: 50
+    }
+]
+
+app.get('/beers', (request, response) =>{
+    response.json({
+        succes: true,
+        data: {
+            beers: beers
+        }
+    })
+})
+
+app.get('/beers/:id', (request, response) => {
+    const id = request.params.id
+    console.log(request.params.id)
+    getBeerById = beers.filter((beer) => {
+        return beer.id === id
+    })
+    response.json({
+        success: true,
+        message: 'Selected Beer',
+        data: {
+            getBeerById
+        }
+    })
+})
+
+app.post('/beers', (request, response) =>{
+    beers.push(request.body)
+    response.json({
+        succes: true,
+        data: {
+            beers: beers
+        }
+    })
+})
+
+app.patch('/beers/:id', (request, response) => {
+    const id = request.params.id
+    beers = beers.map((beer) => {
+        if (beer.id === id ) {
+        beer.marca = request.body.marca
+        }
+        return beer
+    })
+    response.json({
+        success: true,
+        message: 'Beer updated',
+        data: {
+            beers
+        }
+    })
+})
+
+app.delete('/beers/:id', (request, response) => {
+    const id = request.params.id
+    beers = beers.filter((beer) => {
+        return beer.id !== id
+    })
+    response.json({
+        success: true,
+        message: 'beer deleted',
+        data: {
+            beers
+        }
+    })
+})
+
+app.listen(port, () => {
+    console.log('Server is listening in port 8080')
+})
